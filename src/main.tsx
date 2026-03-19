941a64f29d0bf28ddd42466c859115337b5f7b12
KNJ86 <dakggum@gmail.com>
2026-03-19 13:01:07 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+// src/main.tsx 예시
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx' // App.tsx를 불러옵니다.
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App /> {/* 여기서 App.tsx의 내용이 <div id="root">에 들어갑니다. */}
+  </React.StrictMode>,
+)
